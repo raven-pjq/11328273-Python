14b22d6f59fb45d414c04e6469af4e75caff438e
raven-pjq <epenraven5@gmail.com>
2024-12-25 14:23:11 +0800

    Qt1.py

--- Qt1.py
+++ Qt1.py
@@ -8,22 +8,22 @@ class MainWindow(QWidget):
         self.setWindowTitle("Click!")
         self.resize(400, 300)
 
-        # 建立標籤與按鈕
+        # Create labels and buttons
         self.label = QLabel("Start！", self)
         self.button_cnt = QPushButton("Click here", self)
         self.button_rst = QPushButton("Reset", self)
 
-        # 計數變數
+        # Count variable
         self.counter = 0
 
-        # 設定佈局
+        # Set layout
         layout = QVBoxLayout()
         layout.addWidget(self.label)
         layout.addWidget(self.button_rst)
         layout.addWidget(self.button_cnt)
         self.setLayout(layout)
 
-        # 連接按鈕點擊事件
+        # Connect Button click
         self.button_cnt.clicked.connect(self.increment_counter)
         self.button_cnt.clicked.connect(self.counter_limiter)
         self.button_rst.clicked.connect(self.counter_reset)
@@ -40,7 +40,7 @@ class MainWindow(QWidget):
         self.counter = 0
         self.label.setText(f"Reset!")
 
-# 主程式入口
+# Main program entrance
 if __name__ == "__main__":
     app = QApplication(sys.argv)
     window = MainWindow()
